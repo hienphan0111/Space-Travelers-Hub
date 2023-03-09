0e059ba45cca81506511d2dffb4b868cb1e1ffcb
Joseph david <jodavid444dave@gmail.com>
2023-03-09 07:14:31 +0000

    Implement React conditional rendering syntax

--- src/routes/rockets.route.jsx
+++ src/routes/rockets.route.jsx
@@ -1,9 +1,26 @@
-function Rockets() {
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getRockets } from 'redux/rockets/rockets';
+import Rocket from 'components/Rocket.component';
+
+function Rocket() {
+  const { rockets, status } = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (status === 'update') {
+      dispatch(getRockets());
+    }
+  }, [status]);
+​
   return (
-    <div>
-      <p>This is rockets page</p>
+    <div className="rockets-container">
+      {
+        rockets.map((rocket) => (
+          <Rocket key={rocket.id} rocket={rocket} />
+        ))
+      }
     </div>
   );
 }
-
-export default Rockets;
+​
+export default Rocket;
