5a88a055d1bd497353b20684eba45b13ba8ac3d6
Joseph david <jodavid444dave@gmail.com>
2023-03-09 14:48:31 +0000

    Implement actions and reducers for booking rockets/dragons and joining missions

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -24,18 +24,36 @@ const rocketsSlice = createSlice({
   name: 'rockets',
   initialState,
   reducers: {
-
+    bookRocket: (state, action) => {
+      const { payload } = action;
+      const rockets = state.rockets.map((rocket) => {
+        if (rocket.id === payload) {
+          return { ...rocket, reserved: true };
+        }
+        return rocket;
+      });
+      return { ...rockets, status: 'completed' };
+    },
   },
   extraReducers(builder) {
     builder
       .addCase(getRockets.fulfilled, (state, action) => {
         const { payload } = action;
+        const rockets = payload.map((rocket) => ({
+          id: rocket.id,
+          name: rocket.name,
+          description: rocket.description,
+          imgUrl: rocket.flickr_images[0],
+          reserved: false,
+        }));
         return {
-          rockets: payload,
+          rockets,
           status: 'complete',
         };
       });
   },
 });
+
+export const { bookRocket } = rocketsSlice.actions;
 
 export default rocketsSlice.reducer;
